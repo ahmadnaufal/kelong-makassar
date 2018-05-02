6cc0c6ae01a0eb0476865b380827424ec4005799
Ahmad N. F <fahmadnaufal@gmail.com>
2018-05-02 12:49:40 +0700

    optimize imports

--- app/src/main/java/id/kelongmakassar/kelongmakassar/ui/track/list/TrackListActivity.java
+++ app/src/main/java/id/kelongmakassar/kelongmakassar/ui/track/list/TrackListActivity.java
@@ -2,8 +2,8 @@ package id.kelongmakassar.kelongmakassar.ui.track.list;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
