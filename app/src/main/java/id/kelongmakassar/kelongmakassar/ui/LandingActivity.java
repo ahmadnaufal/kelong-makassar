9f388f6d3f104dcb2a42471c4d227bb6519ddbc4
Ahmad N. F <fahmadnaufal@gmail.com>
2018-05-19 16:53:59 +0700

    refactor landing ui, add games activity

--- app/src/main/java/id/kelongmakassar/kelongmakassar/ui/LandingActivity.java
+++ app/src/main/java/id/kelongmakassar/kelongmakassar/ui/LandingActivity.java
@@ -13,6 +13,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import id.kelongmakassar.kelongmakassar.R;
+import id.kelongmakassar.kelongmakassar.ui.games.GamesActivity;
 import id.kelongmakassar.kelongmakassar.ui.track.list.TrackListActivity;
 import id.kelongmakassar.kelongmakassar.ui.tutorial.TutorialActivity;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -20,11 +21,9 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class LandingActivity extends AppCompatActivity {
 
     @BindView(R.id.scrollview_help) ScrollView mHelpScrollView;
-    @BindView(R.id.image_show_help) ImageView mShowHelpImageView;
     @BindView(R.id.layout_buttons) LinearLayout mButtonsLayout;
-
-    private boolean isHelpShown;
-    private boolean isMenuShown;
+    @BindView(R.id.layout_container) LinearLayout mContainerLayout;
+    @BindView(R.id.image_pull_down) ImageView mPullDownImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,24 +40,21 @@ public class LandingActivity extends AppCompatActivity {
     }
 
     private void initLayout() {
-        isHelpShown = false;
-        isMenuShown = false;
+        mHelpScrollView.setVisibility(View.VISIBLE);
+        mButtonsLayout.setVisibility(View.VISIBLE);
 
-        mHelpScrollView.setVisibility(View.GONE);
-        mButtonsLayout.setVisibility(View.GONE);
+        mContainerLayout.setVisibility(View.GONE);
     }
 
-    @OnClick(R.id.image_show_help)
-    void onHelpClick() {
-        isHelpShown = !isHelpShown;
-        mHelpScrollView.setVisibility(isHelpShown ? View.VISIBLE : View.GONE);
-        mShowHelpImageView.setImageResource(isHelpShown ? R.drawable.ic_info_kelong_selected : R.drawable.ic_info_kelong);
+    @OnClick(R.id.image_pull_down)
+    void onPullDownClick() {
+        mContainerLayout.setVisibility(View.VISIBLE);
+        mPullDownImageView.setVisibility(View.GONE);
     }
 
     @OnClick(R.id.button_toggle_menu)
     void onToggleMenu() {
-        isMenuShown = !isMenuShown;
-        mButtonsLayout.setVisibility(isMenuShown ? View.VISIBLE : View.GONE);
+        mHelpScrollView.setVisibility(View.GONE);
     }
 
     @OnClick(R.id.button_kelong)
@@ -75,6 +71,7 @@ public class LandingActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_games)
     void onGamesButtonClick() {
-        // TODO create intent to games activity
+        Intent intent = GamesActivity.getStartIntent(this);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/id/kelongmakassar/kelongmakassar/ui/games/GamesActivity.java
@@ -0,0 +1,36 @@
+package id.kelongmakassar.kelongmakassar.ui.games;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import id.kelongmakassar.kelongmakassar.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class GamesActivity extends AppCompatActivity {
+
+    public static Intent getStartIntent(Context context) {
+        Intent intent = new Intent(context, GamesActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @OnClick(R.id.image_back)
+    void onBackClick() {
+        super.onBackPressed();
+    }
+}
