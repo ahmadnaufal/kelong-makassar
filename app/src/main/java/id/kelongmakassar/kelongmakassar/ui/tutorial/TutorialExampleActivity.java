e7ed126767cc78a29a8355be22d305a0044eddf7
Ahmad N. F <fahmadnaufal@gmail.com>
2018-05-19 22:30:52 +0700

    optimize imports

--- app/src/main/java/id/kelongmakassar/kelongmakassar/ui/tutorial/TutorialExampleActivity.java
+++ app/src/main/java/id/kelongmakassar/kelongmakassar/ui/tutorial/TutorialExampleActivity.java
@@ -2,8 +2,8 @@ package id.kelongmakassar.kelongmakassar.ui.tutorial;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
